6d561f57d14fda15fec2a3385d1b3baeb3372533
woghd111234 <jaehong243587@gmail.com>
2024-09-20 00:19:01 +0900

    api 키 저장 모델 작성

--- user/models.py
+++ user/models.py
@@ -1,6 +1,22 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.conf import settings
+from cryptography.fernet import Fernet
+from django.utils.crypto import get_random_string
 
-class User(AbstractUser):
-    # 추가 필드가 필요하면 여기에 추가
+class User(AbstractUser): # 기본 유저 모델
     pass
+
+class APIKey(models.Model): # Api 키 저장 모델
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    api_key_encrypted = models.TextField()
+
+    def set_api_key(self, raw_key):
+        cipher_suite = Fernet(settings.SECRET_KEY)  # 대칭키 암호화
+        encrypted_key = cipher_suite.encrypt(raw_key.encode())
+        self.api_key_encrypted = encrypted_key.decode()
+
+    def get_api_key(self):
+        cipher_suite = Fernet(settings.SECRET_KEY)
+        decrypted_key = cipher_suite.decrypt(self.api_key_encrypted.encode())
+        return decrypted_key.decode()

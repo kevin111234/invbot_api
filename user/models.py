c044a7518f8c97d214b3e0fb052b0ac5460de960
woghd111234 <jaehong243587@gmail.com>
2024-09-20 13:20:16 +0900

    api 모델 삭제

--- user/models.py
+++ user/models.py
@@ -6,17 +6,3 @@ from django.utils.crypto import get_random_string
 
 class User(AbstractUser): # 기본 유저 모델
     pass
-
-class APIKey(models.Model): # Api 키 저장 모델
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    api_key_encrypted = models.TextField()
-
-    def set_api_key(self, raw_key):
-        cipher_suite = Fernet(settings.SECRET_KEY)  # 대칭키 암호화
-        encrypted_key = cipher_suite.encrypt(raw_key.encode())
-        self.api_key_encrypted = encrypted_key.decode()
-
-    def get_api_key(self):
-        cipher_suite = Fernet(settings.SECRET_KEY)
-        decrypted_key = cipher_suite.decrypt(self.api_key_encrypted.encode())
-        return decrypted_key.decode()
